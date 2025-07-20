3ab29fc1ff5ed76158998e899ffe09760da5ad20
vinaanurfauziah <vinaanurfauziah@gmail.com>
2025-07-21 06:33:48 +0700

    hapus komponen tdk terpakai

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -1,8 +1,7 @@
-import ApplicationLogo from '@/Components/Common/ApplicationLogo';
 import Dropdown from '@/Components/Common/Dropdown';
 import NavLink from '@/Components/Common/NavLink';
 import ResponsiveNavLink from '@/Components/Common/ResponsiveNavLink';
-import { Link, usePage } from '@inertiajs/react';
+import { usePage } from '@inertiajs/react';
 import { useState } from 'react';
 
 export default function AuthenticatedLayout({ header, children }) {
@@ -17,11 +16,7 @@ export default function AuthenticatedLayout({ header, children }) {
                 <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                     <div className="flex h-16 justify-between">
                         <div className="flex">
-                            <div className="flex shrink-0 items-center">
-                                <Link href="/">
-                                    <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800" />
-                                </Link>
-                            </div>
+                            <div className="flex shrink-0 items-center"></div>
 
                             <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                                 <NavLink
