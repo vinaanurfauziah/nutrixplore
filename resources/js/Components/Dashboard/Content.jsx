8ed97567cda739c0bc384d6ba38860e2d2635d48
vinaanurfauziah <vinaanurfauziah@gmail.com>
2025-07-29 20:59:13 +0700

    paudafj

--- resources/js/Components/Dashboard/Content.jsx
+++ resources/js/Components/Dashboard/Content.jsx
@@ -1,13 +1,13 @@
+import { usePage } from '@inertiajs/react';
 import ArticleTable from './Article/ArticleTable';
 import RecipeTable from './Recipe/RecipeTable';
 import StatBox from './Stat/StatBox';
-import { usePage } from '@inertiajs/react';
 
 export default function Content() {
-        const { totalResep } = usePage().props;
+    const { totalResep } = usePage().props;
     return (
         <div className="space-y-12">
-                  <section className="grid grid-cols-2 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+            <section className="grid grid-cols-2 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
                 <StatBox title="Total Resep" value={totalResep} />
                 <StatBox title="Total Artikel" value="20" />
             </section>
